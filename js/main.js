f5f45588fef3c3e735622c0a7becaab623f48670
zare926 <ilove.boc926@gmail.com>
2020-06-21 01:53:55 +0900

    Jsでimgの配置、ランダムに切り替え

--- js/main.js
+++ js/main.js
@@ -12,6 +12,39 @@
       this.stop = document.createElement('div');
       this.stop.textContent = 'STOP';
       this.stop.classList.add('stop');
+
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+      
+
+      const main = document.querySelector('main');
+      main.appendChild(section);
+    }
+
+    getRandomImage(){
+      const images = [
+        'img/seven.png',
+        'img/bell.png',
+        'img/cherry.png',
+      ];
+      return images[Math.floor(Math.random() * images.length)];
+    }
+
+    spin(){
+      this.img.src = this.getRandomImage();
     }
   }
+
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
 }
